f22b47d080a04d02147c96a57eefe6fc9711eda1
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2025-12-26 22:02:25 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {}
+variable "region" { default = "us-central1" }
+variable "zone"   { default = "us-central1-a" }
+
+variable "vm_name" {}
+variable "db_user" {}
+variable "db_pass" {}
+variable "bucket_name" {}
+variable "pipeline_name" {}
+variable "bindplane_token" {}
