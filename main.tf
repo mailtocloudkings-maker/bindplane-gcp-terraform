cf1d0c2ea02566a06c38be1d5bab18677ddbe7ef
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2025-12-28 17:22:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,15 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2"
-    }
   }
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+  credentials = file(var.credentials_file)
 }
