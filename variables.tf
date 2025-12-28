6cdf5e85c2dc674ab60adb9ae7f3386e6537ed24
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2025-12-28 18:12:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,10 @@
 variable "project_id" {}
-variable "region" { default = "us-central1" }
-variable "zone"   { default = "us-central1-a" }
-variable "credentials_file" {
-  description = "Path to GCP service account key file"
-  type        = string
-}
-
+variable "region" {}
+variable "zone" {}
 variable "vm_name" {}
 variable "db_user" {}
 variable "db_pass" {}
 variable "bp_admin_user" {}
 variable "bp_admin_pass" {}
 variable "bucket_name" {}
-
-
+variable "credentials_file" {}
