706d7b0896198474eef6a6615fe6329801e3d11d
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2025-12-28 18:10:09 +0530

    Delete gco.tf

--- gco.tf
+++
@@ -1,7 +0,0 @@
-resource "google_project_service" "monitoring" {
-  service = "monitoring.googleapis.com"
-}
-
-resource "google_project_service" "logging" {
-  service = "logging.googleapis.com"
-}
