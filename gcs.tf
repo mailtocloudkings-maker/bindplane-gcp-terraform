498c39c63254ffd44c88778bdf57487037bb225c
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-06 11:20:44 +0530

    Update gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -1,9 +1,6 @@
 resource "google_storage_bucket" "bindplane_logs" {
   name                        = var.bucket_name
-  location                    = "US-CENTRAL1"
+  location                    = var.region
   force_destroy               = true
   uniform_bucket_level_access = true
-  lifecycle {
-    prevent_destroy = false
-  }
 }
