fd9896d420873f76d0f4897ef25971b006e5551c
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-06 12:06:48 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,19 @@
 output "vm_external_ip" {
-  description = "The external IP of the BindPlane VM"
+  description = "External IP of BindPlane VM"
   value       = google_compute_instance.bindplane_vm.network_interface[0].access_config[0].nat_ip
 }
 
 output "gcs_bucket_name" {
-  description = "The name of the GCS bucket created for logs"
+  description = "GCS bucket for BindPlane logs"
   value       = google_storage_bucket.bindplane_logs.name
+}
+
+output "postgres_status" {
+  description = "PostgreSQL service status on VM"
+  value       = "Use null_resource logs in GitHub Actions for live status"
+}
+
+output "bindplane_status" {
+  description = "BindPlane server service status on VM"
+  value       = "Use null_resource logs in GitHub Actions for live status"
 }
